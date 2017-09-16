33e5bd1825ac4fa9def36cc0e0e602e1cebbabfe
jlondon <jlondon@netflix.com>
2017-09-16 10:35:52 -0700

    init commit

---
+++ Gulpfile.js
@@ -0,0 +1,139 @@
+var gulp = require('gulp'),
+
+  // browserSync
+  browserSync = require('browser-sync').create(),
+  reload = browserSync.reload,
+
+  // CSS
+  sass = require('gulp-sass'),
+  cssnano = require('gulp-cssnano'),
+  sourcemaps = require('gulp-sourcemaps'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  atImport = require('postcss-import'),
+
+  // Utils
+  rename = require('gulp-rename'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  gutil = require('gulp-util'),
+  cp = require('child_process'),
+  notify = require('gulp-notify'),
+  shell = require('gulp-shell'),
+  runSequence = require('run-sequence'),
+  styleGuide = require('postcss-style-guide');
+
+// Constants
+const sourceRoot = '_src/';
+const devRoot = '_dev/';
+const dist = '_dist/';
+
+
+// ------------------------------------------------------
+// CSS
+gulp.task('css', function() {
+  return gulp.src([sourceRoot + 'assets/sass/cropfactor.scss'])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([
+  	autoprefixer({
+  		browsers: ['last 5 versions', 'iOS 8']
+  		})
+  	]))
+    .pipe(rename('escobar.css'))
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(devRoot + 'assets/css/'))
+    .pipe(browserSync.stream({ match: '**/*.css' })) // 'match' is used to prevent reload with sourcemaps file
+    .pipe(notify({ message: '✓ Main CSS complete' }));
+});
+
+
+// ------------------------------------------------------
+// Copy vendor JS
+gulp.task('vendorjs', function() {
+  return gulp.src([sourceRoot + 'assets/js/vendor/*.js'])
+    .pipe(gulp.dest(devRoot + 'assets/js/vendor/'))
+});
+
+// ------------------------------------------------------
+// JS
+gulp.task('js', function() {
+  return gulp.src([sourceRoot + 'assets/js/*.js'])
+    .pipe(concat('cropfactor.js'))
+    .pipe(gulp.dest(devRoot + 'assets/js/'))
+    .pipe(notify({ message: '✓ JS complete' }))
+});
+
+
+
+// ------------------------------------------------------
+// Jekyll
+const child = require('child_process');
+gulp.task('jekyll', () => {
+  const jekyll = child.spawn('jekyll', ['build',
+      '--incremental',
+      '--drafts',
+      '--unpublished',
+      '--future'
+    ]);
+
+    const jekyllLogger = (buffer) => {
+        buffer.toString()
+            .split(/\n/)
+            .forEach((message) => gutil.log('Jekyll: ' + message));
+    };
+
+    jekyll.stdout.on('data', jekyllLogger);
+    jekyll.stderr.on('data', jekyllLogger);
+});
+
+
+
+// ------------------------------------------------------
+// Reload
+gulp.task('reload', function() {
+	browserSync.reload();
+});
+
+
+
+// ------------------------------------------------------
+// Serve - browserSync
+gulp.task('serve', function() {
+
+    browserSync.init({
+        port: 3726,
+        notify: false,
+        server: {
+            baseDir: devRoot
+        }
+    });
+
+	// watch the CSS
+	gulp.watch([sourceRoot + '**/*.scss'], ['css']);
+
+	// watch the critical css and reload the browser (because it's injected into the html head)
+	//gulp.watch(sourceRoot + 'assets/_criticalcss/criticalcss.scss', ['criticalcss', 'jekyll']);
+
+	// watch html and markdown to run jekyll
+	gulp.watch([
+		sourceRoot + '**/*.html',
+		sourceRoot + '**/*.md',
+	], ['jekyll']);
+
+  // gulp.watch([
+  //   sourceRoot + '/_data/**/*.json'
+  // ], ['jekyll', 'reload']);
+
+  // watch the html in the dist folder to refresh browser
+  // waits for jekyll to finish building
+  gulp.watch([
+    devRoot + '**/*.html'
+  ], ['reload']);
+
+  gulp.watch([
+    sourceRoot + '**/*.js'
+  ], ['js', 'reload']);
+});
+
+gulp.task('default', ['css', 'js', 'jekyll', 'serve']);
---
+++ assets/js/cropfactor.js
@@ -0,0 +1,109 @@
+$(document).ready(function () {
+  console.log('ready');
+
+  var cameraPicker = $('.select--camera-picker');
+  var lensPicker = $('.select--lens-picker');
+
+  // remove all the optgroups from the lens picker
+  function clearLensPicker() {
+    lensPicker.find('optgroup').hide();
+  }
+
+  cameraPicker.on('change', function() {
+    var cameraSensor = $(this).find(':selected').attr('data-sensor');
+        cameraCrop = $(this).find(':selected').attr('data-crop');
+        cameraWidth = $(this).find(':selected').attr('data-mmwidth');
+        cameraHeight = $(this).find(':selected').attr('data-mmheight');
+
+        cameraOptGroupId = cameraPicker.find(':selected').closest('optgroup').attr('data-groupid');
+        lensOptGroupId = lensPicker.find('optgroup').attr('data-groupid');
+
+        cameraDisplaySensor = $('.camera-details--sensor');
+        cameraDisplayCrop = $('.camera-details--crop');
+        cameraDisplayWidthHeight = $('.camera-details--width-height');
+
+    // enable the lens picker
+    lensPicker.removeAttr('disabled');
+
+    clearLensPicker();
+
+    // display the appropriate lenses
+    lensPicker.find('optgroup[data-groupid=' + cameraOptGroupId +']').show();
+
+    cameraDisplaySensor.text(cameraSensor);
+    cameraDisplayCrop.text(cameraCrop);
+    cameraDisplayWidthHeight.html(cameraWidth + '<span>mm</span> x ' + cameraHeight + '<span>mm</span>');
+
+    displayLists();
+  });
+
+
+
+  lensPicker.on('change', function() {
+    var fstop = $(this).find(':selected').attr('data-fstop');
+        resultsFstop = $('.results--fstop');
+    resultsFstop.html('<span>f</span>' + fstop);
+
+    doTheCalc();
+
+    displayLists();
+  });
+
+  function displayLists() {
+
+    var cameraOptGroupId = cameraPicker.find(':selected').closest('optgroup').attr('data-groupid');
+        lensOptGroupId = lensPicker.find(':selected').closest('optgroup').attr('data-groupid');
+
+
+
+    console.log('camera group id' + cameraOptGroupId);
+    console.log('lens group id' + lensOptGroupId);
+  }
+
+  function doTheCalc() {
+
+    var cameraCrop = cameraPicker.find(':selected').attr('data-crop');
+        focalLength = lensPicker.find(':selected').attr('data-focal-length');
+
+        calcResult = cameraCrop * focalLength;
+        calcResult = Math.round(calcResult);
+        newFocalLength = $('.crop-result--value');
+
+    newFocalLength.text(calcResult);
+
+    console.log('camera crop', cameraCrop);
+    console.log('focal length', focalLength);
+    console.log('calc result', calcResult);
+
+    if (cameraOptGroupId != null && lensOptGroupId != null) {
+      console.log('both cam and lens have values');
+    }
+  }
+
+  // var cameraPicker = $('.select--camera-picker');
+  //     focalLength = $('.input--focal-length');
+  //     cropSize = $('.input--crop-size');
+  //     calcButton = $('.button--calculate');
+  //
+  // // get the camera
+  // cameraPicker.on('change', function() {
+  //   var cropVal = $(this).val();
+  //   console.log(cropVal);
+  // });
+  //
+  // function doTheCalc() {
+  //
+  //   var cropVal = cameraPicker.val();
+  //       focalLengthVal = focalLength.val();
+  //       cropSizeVal = cropSize.val();
+  //
+  //   var result = cropVal * focalLengthVal;
+  //
+  //   cropSize.val(Math.round(result));
+  // }
+  //
+  // calcButton.on('click', function() {
+  //   doTheCalc();
+  // });
+
+});
